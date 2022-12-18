523a29ad05eb3ed9d92e3be924711f8d7e724050
Manish <mshakya.dev@gmail.com>
2022-12-18 21:34:48 +0530

    some changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const product = require("./routes/productRoute");
 const errormiddleware = require("./middleware/error");
 const user = require("./routes/userRoute");
+const product = require("./routes/productRoute");
+const order = require("./routes/orderRoute")
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //middleware for error
 app.use(errormiddleware);
